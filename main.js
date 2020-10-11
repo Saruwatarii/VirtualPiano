8fa6bc90a7583e043e295d2603da8ac3d7bbd25f
Tim Aadland <potetstapp@gmail.com>
2020-10-11 23:56:07 +0200

    Changed the event handler so that it produce sound instead of a console log. So far the virtual piano has 6 keys with working sounds.

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-
 document.addEventListener("keydown", function(event) {
-    if (event.code === "KeyA")
-        console.log("The 'A' key is pressed.");
-    else if (event.code === "KeyS")
-        console.log("The 'S' key is pressed.");
-    else if (event.code === "KeyD")
-        console.log("The 'D' key is pressed.");
-    else if (event.code === "KeyF")
-        console.log("The 'F' key is pressed.");
-    else if (event.code === "KeyG")
-        console.log("The 'G' key is pressed.");
-    else if (event.code === "KeyH")
-        console.log("The 'H' key is pressed.");
-    else if (event.code === "KeyJ")
-        console.log("The 'J' key is pressed.");
+    if (event.code === "KeyA"){
+        let audio = new Audio('music/A.mp3');
+        audio.play();}
+    else if (event.code === "KeyS"){
+        let audio = new Audio('music/S.mp3');
+        audio.play();}
+    else if (event.code === "KeyD"){
+        let audio = new Audio('music/D.mp3');
+        audio.play();}
+    else if (event.code === "KeyF"){
+        let audio = new Audio('music/F.mp3');
+        audio.play();}
+    else if (event.code === "KeyG"){
+        let audio = new Audio('music/G.mp3');
+        audio.play();}
+    else if (event.code === "KeyH"){
+        let audio = new Audio('music/H.mp3');
+        audio.play();}
+    else if (event.code === "KeyJ"){
+        let audio = new Audio('music/J.mp3');
+        audio.play();}
     else
         console.log("Not hitting the piano keystrokes")
 });
