bbdd68253291d61aa3ffdf11dd3c4942fe26245c
Tim Aadland <potetstapp@gmail.com>
2020-10-12 08:42:57 +0200

    Created 5 black keys, and binded them with sound. And position them approximately as they normally look on a real instrument

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Sound for the white keys
 document.addEventListener("keydown", function(event) {
     if (event.code === "KeyA"){
         let audio = new Audio('music/A.mp3');
@@ -21,5 +22,25 @@ document.addEventListener("keydown", function(event) {
         let audio = new Audio('music/J.mp3');
         audio.play();}
     else
+        console.log("Not hitting the piano keystrokes")
+});
+// Sound for the black keys
+document.addEventListener("keydown", function(event){
+    if (event.code === "KeyW"){
+        let audio = new Audio('music/W.mp3');
+        audio.play();
+    } else if (event.code === "KeyE"){
+        let audio = new Audio('music/E.mp3');
+        audio.play();
+    } else if (event.code === "KeyT"){
+        let audio = new Audio('music/T.mp3');
+        audio.play();
+    } else if (event.code === "KeyY"){
+        let audio = new Audio('music/Y.mp3');
+        audio.play();
+    } else if (event.code === "KeyU") {
+        let audio = new Audio('music/U.mp3');
+        audio.play();
+    } else
         console.log("Not hitting the piano keystrokes")
 });
